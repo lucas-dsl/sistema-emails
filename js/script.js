213a2892d838075c2e3fceee2d668295c653a39a
Lucas <silvalucas71743@gmail.com>
2026-02-08 21:22:25 -0300

    adc: funcionalidade para modo de análise do email

--- js/script.js
+++ js/script.js
@@ -1,10 +1,37 @@
-const btn = document.getElementById("switch-theme");
+// TEMA (Light / Dark)
+const themeBtn = document.getElementById("switch-theme");
 const sun = document.querySelector(".theme-icon-sol");
 const moon = document.querySelector(".theme-icon-lua");
 
-btn.addEventListener("click", () => {
+themeBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark-theme");
 
   sun.classList.toggle("theme-active");
   moon.classList.toggle("theme-active");
 });
+
+// MODO DE ANÁLISE DO EMAIL
+const writeBtn = document.querySelector(".btn-write");
+const uploadBtn = document.querySelector(".btn-upload");
+
+const writeForm = document.querySelector(".mode-write");
+const uploadForm = document.querySelector(".mode-upload");
+
+function activateWrite() {
+  writeForm.classList.remove("is-hidden");
+  uploadForm.classList.add("is-hidden");
+
+  writeBtn.classList.add("mode-switch-active");
+  uploadBtn.classList.remove("mode-switch-active");
+}
+
+function activateUpload() {
+  uploadForm.classList.remove("is-hidden");
+  writeForm.classList.add("is-hidden");
+
+  uploadBtn.classList.add("mode-switch-active");
+  writeBtn.classList.remove("mode-switch-active");
+}
+
+writeBtn.addEventListener("click", activateWrite);
+uploadBtn.addEventListener("click", activateUpload);
