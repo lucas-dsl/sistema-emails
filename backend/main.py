287584ef1d52521b7ba391c97ae6f8484c7a5792
Lucas <silvalucas71743@gmail.com>
2026-02-09 01:46:23 -0300

    remove: testes sem utilização por enquanto

--- backend/main.py
+++ backend/main.py
@@ -22,38 +22,3 @@ app.add_middleware(
 @app.get("/")
 def home():
     return {"status": "Backend rodando 🚀"}
-
-
-# ROTA ANALISAR TEXTO
-@app.post("/analyze-text")
-async def analyze_text(data: dict):
-
-    texto = data.get("text")
-
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role": "system", "content": "Você é um analisador de emails."},
-            {"role": "user", "content": texto}
-        ]
-    )
-
-    return {"result": response.choices[0].message.content}
-
-
-# ROTA ANALISAR ARQUIVO
-@app.post("/analyze-file")
-async def analyze_file(file: UploadFile = File(...)):
-
-    conteudo = await file.read()
-    texto = conteudo.decode("utf-8")
-
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role": "system", "content": "Analise este arquivo:"},
-            {"role": "user", "content": texto}
-        ]
-    )
-
-    return {"result": response.choices[0].message.content}
